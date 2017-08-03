c642ad5176db51f269bb9409cd92b15a24364a32
erichua <erichua@erichuas-MacMini.local>
2017-08-03 22:44:53 +0800

    fix size

--- SearchList.js
+++ SearchList.js
@@ -414,9 +414,7 @@ export default class SearchList extends Component {
   }
 
   renderAlphaSection (sectionData, sectionID) {
-    return (
-      <Text style={{color: '#171a23', fontSize: 11}}>{sectionID}</Text>
-    )
+    return (<Text style={{ color: '#171a23', fontSize: 11, width: 36 }}>{sectionID}</Text>))
   }
 
   renderSeparator (sectionID,
